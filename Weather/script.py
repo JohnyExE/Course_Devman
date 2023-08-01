8c9f19f783f2e2929a74831d63f6f758dad70420
Johny <processor.exe@gmail.com>
2023-08-01 10:21:43 +0300

    Done

--- Weather/script.py
+++ Weather/script.py
@@ -6,13 +6,11 @@ urls = ['https://wttr.in/London?nTqu&lang=en',
         'https://wttr.in/Шереметьево?nTqu&lang=en',
         'https://wttr.in/Череповец?nTqM&lang=ru']
 
-
-def request(weather_url: str) -> object:
-    """Получаем ссылку на погоду и город. Возвращаем объект - погоду в этом городе"""
-    return requests.get(weather_url)
-
+if __name__ == '__main__':
+    def request(weather_url: str) -> object:
+        """Получаем ссылку на погоду. Возвращаем объект - погоду"""
+        return requests.get(weather_url)
 
 for city in urls:
     print(request(city).text)
-
 
